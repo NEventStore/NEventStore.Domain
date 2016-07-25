becb69dc6191bed09d819cc2a75e3b9bbd9565a6
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2016-07-25 11:02:55 +0200

    fixed assemblyinfo attributes

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("5.2.0.0")]
-[assembly: AssemblyFileVersion("5.2.0.0")]
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
+[assembly: AssemblyInformationalVersion("6.0.0.0")]
