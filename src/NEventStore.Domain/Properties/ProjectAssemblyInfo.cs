5aa3723cab2a099b750064837786c33467ab5e9e
Alessandro Giorgetti <Alessandro.giorgetti@live.com>
2017-08-17 16:20:43 +0200

    Assembly information attrs are now read from files again

--- src/NEventStore.Domain/Properties/ProjectAssemblyInfo.cs
+++ src/NEventStore.Domain/Properties/ProjectAssemblyInfo.cs
@@ -1,8 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-#if !NETSTANDARD1_6
 [assembly: AssemblyTitle("NEventStore.Domain")]
 [assembly: AssemblyDescription("")]
-#endif
 [assembly: Guid("c55ee391-875e-43ea-b724-6f58a5d68985")]
